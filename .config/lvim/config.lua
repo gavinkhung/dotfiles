bf81592cddf6e4065b04870dc6fbf49228475bdd
Gavin Hung <32819629+gavinkhung@users.noreply.github.com>
2022-12-26 14:13:35 -0800

    added keybindings and plugins

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,32 +1,20 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.ignorecase = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.undodir = vim.fn.stdpath "cache" .. "/undo"
+vim.opt.undofile = true
 
--- vim.g.tokyonight_style = "storm"
--- lvim.colorscheme = "tokyonight"
 lvim.colorscheme = "kanagawa"
 
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.lint_on_save = true
--- lvim.colorscheme = "tokyonight"
--- lvim.colorscheme = "codedark"
 lvim.transparent_window = true
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -36,20 +24,14 @@ lvim.keys.normal_mode["<C-f>"] = ":NvimTreeRefresh | NvimTreeFindFileToggle<CR>"
 
 lvim.keys.normal_mode["<S-q>"] = ":BufferKill<CR>"
 
-lvim.keys.normal_mode["<C-p>"] = ":Telescope find_files<CR>"
-lvim.keys.normal_mode["<C-S-g>"] = ":DiffviewFileHistory %<CR>"
+lvim.keys.normal_mode["<C-p>"] = ":Telescope git_files<CR>"
+lvim.keys.normal_mode["<C-S-f>"] = ":Telescope live_grep<CR>"
+lvim.keys.normal_mode["<C-S-g>"] = ":DiffviewOpen<CR>"
+lvim.keys.normal_mode["<C-S-b>"] = ":DiffviewClose<CR>"
 
 
 lvim.keys.normal_mode["<s-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<s-h>"] = ":BufferLineCyclePrev<CR>"
-
--- lvim.builtin.which_key.mappings["S"] = {
---   name = "Session",
---   c = { "<cmd>lua require('folke/persistence.nvim').load()<cr>", "Restore last session for current dir" },
---   l = { "<cmd>lua require('folke/persistence.nvim').load({ last = true })<cr>", "Restore last session" },
---   Q = { "<cmd>lua require('folke/persistence.nvim').stop()<cr>", "Quit without saving session" },
--- }
-
 
 lvim.builtin.which_key.mappings["S"] = {
   name = "Session",
@@ -65,21 +47,21 @@ lvim.builtin.which_key.mappings["S"] = {
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -93,8 +75,6 @@ lvim.builtin.which_key.mappings["S"] = {
 --   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 -- }
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 -- lvim.builtin.alpha.dashboard.section.header.val = {
@@ -125,24 +105,6 @@ lvim.builtin.alpha.dashboard.section.header.val = {
   "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
   "                                   ",
 }
-
--- lvim.builtin.alpha.dashboard.section.header.val = {
---   "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
---   "⣿⣿⣄⣽⠟⠉⠀⠀⠉⠙⠛⠛⠛⠛⢻⡿⠁⠀⠀⠀⠀⠈⠻⢿⠛⠉⠛⣿⣿⣿",
---   "⣿⣿⣿⢋⣠⣤⣴⣦⣄⡀⠀⠀⣀⣀⣿⠁⠀⠀⠀⠀⠀⣶⠀⠀⢀⡀⠀⠘⣿⣿",
---   "⣿⣿⣿⡿⠿⠛⠋⣁⣭⣿⣶⠟⠋⢹⣿⣴⣾⣿⣿⣶⣾⣯⣶⣾⣿⣿⣿⣶⣽⣿",
---   "⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⡇⠀⢠⣿⣍⣉⡉⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
---   "⣿⡿⠛⠛⠛⠛⠛⢿⡿⠟⢁⠀⠀⠙⠿⠿⠿⠿⠿⠿⢿⣿⠟⠉⠉⠙⠋⠉⠙⣿",
---   "⣿⣷⣶⣾⣿⣷⣄⣼⣿⣾⣿⣿⣶⣦⣤⣄⣀⠀⠀⠀⣾⡷⠞⠻⣿⣾⠟⣿⣿⣿",
---   "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠁⠀⠀⠐⠛⠋⣰⣿⣿⣿",
---   "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣁⣀⣀⣀⣠⣶⣾⣿⣿⣿⣿⣿",
---   "⣿⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣿",
---   "⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿",
---   "⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿",
---   "⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿",
---   "⣿⣿⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣿⣿",
---   "⣿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣾⣿⣿",
--- }
 
 lvim.builtin.terminal.active = true
 lvim.builtin.telescope.defaults.file_ignore_patterns = {
@@ -286,6 +248,13 @@ lvim.plugins = {
       })
     end
   },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
   { "tpope/vim-surround", },
   { "windwp/nvim-ts-autotag",
     config = function()
@@ -425,4 +394,153 @@ lvim.plugins = {
       }
     end
   },
+  {
+    "pwntester/octo.nvim",
+    event = "BufRead",
+    config = function()
+      require "octo".setup({
+        default_remote = { "upstream", "origin" }; -- order to try remotes
+        ssh_aliases = {}, -- SSH aliases. e.g. `ssh_aliases = {["github.com-work"] = "github.com"}`
+        reaction_viewer_hint_icon = ""; -- marker for user reactions
+        user_icon = " "; -- user icon
+        timeline_marker = ""; -- timeline marker
+        timeline_indent = "2"; -- timeline indentation
+        right_bubble_delimiter = ""; -- bubble delimiter
+        left_bubble_delimiter = ""; -- bubble delimiter
+        github_hostname = ""; -- GitHub Enterprise host
+        snippet_context_lines = 4; -- number or lines around commented lines
+        issues = {
+          order_by = { -- criteria to sort results of `Octo issue list`
+            field = "CREATED_AT", -- either COMMENTS, CREATED_AT or UPDATED_AT (https://docs.github.com/en/graphql/reference/enums#issueorderfield)
+            direction = "DESC" -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
+          }
+        },
+        pull_requests = {
+          order_by = { -- criteria to sort the results of `Octo pr list`
+            field = "CREATED_AT", -- either COMMENTS, CREATED_AT or UPDATED_AT (https://docs.github.com/en/graphql/reference/enums#issueorderfield)
+            direction = "DESC" -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
+          },
+          always_select_remote_on_create = "false" -- always give prompt to select base remote repo when creating PRs
+        },
+        file_panel = {
+          size = 10, -- changed files panel rows
+          use_icons = true -- use web-devicons in file panel (if false, nvim-web-devicons does not need to be installed)
+        },
+        mappings = {
+          issue = {
+            close_issue = { lhs = "<space>ic", desc = "close issue" },
+            reopen_issue = { lhs = "<space>io", desc = "reopen issue" },
+            list_issues = { lhs = "<space>il", desc = "list open issues on same repo" },
+            reload = { lhs = "<C-r>", desc = "reload issue" },
+            open_in_browser = { lhs = "<C-b>", desc = "open issue in browser" },
+            copy_url = { lhs = "<C-y>", desc = "copy url to system clipboard" },
+            add_assignee = { lhs = "<space>aa", desc = "add assignee" },
+            remove_assignee = { lhs = "<space>ad", desc = "remove assignee" },
+            create_label = { lhs = "<space>lc", desc = "create label" },
+            add_label = { lhs = "<space>la", desc = "add label" },
+            remove_label = { lhs = "<space>ld", desc = "remove label" },
+            goto_issue = { lhs = "<space>gi", desc = "navigate to a local repo issue" },
+            add_comment = { lhs = "<space>ca", desc = "add comment" },
+            delete_comment = { lhs = "<space>cd", desc = "delete comment" },
+            next_comment = { lhs = "]c", desc = "go to next comment" },
+            prev_comment = { lhs = "[c", desc = "go to previous comment" },
+            react_hooray = { lhs = "<space>rp", desc = "add/remove 🎉 reaction" },
+            react_heart = { lhs = "<space>rh", desc = "add/remove ❤️ reaction" },
+            react_eyes = { lhs = "<space>re", desc = "add/remove 👀 reaction" },
+            react_thumbs_up = { lhs = "<space>r+", desc = "add/remove 👍 reaction" },
+            react_thumbs_down = { lhs = "<space>r-", desc = "add/remove 👎 reaction" },
+            react_rocket = { lhs = "<space>rr", desc = "add/remove 🚀 reaction" },
+            react_laugh = { lhs = "<space>rl", desc = "add/remove 😄 reaction" },
+            react_confused = { lhs = "<space>rc", desc = "add/remove 😕 reaction" },
+          },
+          pull_request = {
+            checkout_pr = { lhs = "<space>po", desc = "checkout PR" },
+            merge_pr = { lhs = "<space>pm", desc = "merge commit PR" },
+            squash_and_merge_pr = { lhs = "<space>psm", desc = "squash and merge PR" },
+            list_commits = { lhs = "<space>pc", desc = "list PR commits" },
+            list_changed_files = { lhs = "<space>pf", desc = "list PR changed files" },
+            show_pr_diff = { lhs = "<space>pd", desc = "show PR diff" },
+            add_reviewer = { lhs = "<space>va", desc = "add reviewer" },
+            remove_reviewer = { lhs = "<space>vd", desc = "remove reviewer request" },
+            close_issue = { lhs = "<space>ic", desc = "close PR" },
+            reopen_issue = { lhs = "<space>io", desc = "reopen PR" },
+            list_issues = { lhs = "<space>il", desc = "list open issues on same repo" },
+            reload = { lhs = "<C-r>", desc = "reload PR" },
+            open_in_browser = { lhs = "<C-b>", desc = "open PR in browser" },
+            copy_url = { lhs = "<C-y>", desc = "copy url to system clipboard" },
+            goto_file = { lhs = "gf", desc = "go to file" },
+            add_assignee = { lhs = "<space>aa", desc = "add assignee" },
+            remove_assignee = { lhs = "<space>ad", desc = "remove assignee" },
+            create_label = { lhs = "<space>lc", desc = "create label" },
+            add_label = { lhs = "<space>la", desc = "add label" },
+            remove_label = { lhs = "<space>ld", desc = "remove label" },
+            goto_issue = { lhs = "<space>gi", desc = "navigate to a local repo issue" },
+            add_comment = { lhs = "<space>ca", desc = "add comment" },
+            delete_comment = { lhs = "<space>cd", desc = "delete comment" },
+            next_comment = { lhs = "]c", desc = "go to next comment" },
+            prev_comment = { lhs = "[c", desc = "go to previous comment" },
+            react_hooray = { lhs = "<space>rp", desc = "add/remove 🎉 reaction" },
+            react_heart = { lhs = "<space>rh", desc = "add/remove ❤️ reaction" },
+            react_eyes = { lhs = "<space>re", desc = "add/remove 👀 reaction" },
+            react_thumbs_up = { lhs = "<space>r+", desc = "add/remove 👍 reaction" },
+            react_thumbs_down = { lhs = "<space>r-", desc = "add/remove 👎 reaction" },
+            react_rocket = { lhs = "<space>rr", desc = "add/remove 🚀 reaction" },
+            react_laugh = { lhs = "<space>rl", desc = "add/remove 😄 reaction" },
+            react_confused = { lhs = "<space>rc", desc = "add/remove 😕 reaction" },
+          },
+          review_thread = {
+            goto_issue = { lhs = "<space>gi", desc = "navigate to a local repo issue" },
+            add_comment = { lhs = "<space>ca", desc = "add comment" },
+            add_suggestion = { lhs = "<space>sa", desc = "add suggestion" },
+            delete_comment = { lhs = "<space>cd", desc = "delete comment" },
+            next_comment = { lhs = "]c", desc = "go to next comment" },
+            prev_comment = { lhs = "[c", desc = "go to previous comment" },
+            select_next_entry = { lhs = "]q", desc = "move to previous changed file" },
+            select_prev_entry = { lhs = "[q", desc = "move to next changed file" },
+            close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
+            react_hooray = { lhs = "<space>rp", desc = "add/remove 🎉 reaction" },
+            react_heart = { lhs = "<space>rh", desc = "add/remove ❤️ reaction" },
+            react_eyes = { lhs = "<space>re", desc = "add/remove 👀 reaction" },
+            react_thumbs_up = { lhs = "<space>r+", desc = "add/remove 👍 reaction" },
+            react_thumbs_down = { lhs = "<space>r-", desc = "add/remove 👎 reaction" },
+            react_rocket = { lhs = "<space>rr", desc = "add/remove 🚀 reaction" },
+            react_laugh = { lhs = "<space>rl", desc = "add/remove 😄 reaction" },
+            react_confused = { lhs = "<space>rc", desc = "add/remove 😕 reaction" },
+          },
+          submit_win = {
+            approve_review = { lhs = "<C-a>", desc = "approve review" },
+            comment_review = { lhs = "<C-m>", desc = "comment review" },
+            request_changes = { lhs = "<C-r>", desc = "request changes review" },
+            close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
+          },
+          review_diff = {
+            add_review_comment = { lhs = "<space>ca", desc = "add a new review comment" },
+            add_review_suggestion = { lhs = "<space>sa", desc = "add a new review suggestion" },
+            focus_files = { lhs = "<leader>e", desc = "move focus to changed file panel" },
+            toggle_files = { lhs = "<leader>b", desc = "hide/show changed files panel" },
+            next_thread = { lhs = "]t", desc = "move to next thread" },
+            prev_thread = { lhs = "[t", desc = "move to previous thread" },
+            select_next_entry = { lhs = "]q", desc = "move to previous changed file" },
+            select_prev_entry = { lhs = "[q", desc = "move to next changed file" },
+            close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
+            toggle_viewed = { lhs = "<leader><space>", desc = "toggle viewer viewed state" },
+          },
+          file_panel = {
+            next_entry = { lhs = "j", desc = "move to next changed file" },
+            prev_entry = { lhs = "k", desc = "move to previous changed file" },
+            select_entry = { lhs = "<cr>", desc = "show selected changed file diffs" },
+            refresh_files = { lhs = "R", desc = "refresh changed files panel" },
+            focus_files = { lhs = "<leader>e", desc = "move focus to changed file panel" },
+            toggle_files = { lhs = "<leader>b", desc = "hide/show changed files panel" },
+            select_next_entry = { lhs = "]q", desc = "move to previous changed file" },
+            select_prev_entry = { lhs = "[q", desc = "move to next changed file" },
+            close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
+            toggle_viewed = { lhs = "<leader><space>", desc = "toggle viewer viewed state" },
+          }
+        }
+      })
+    end,
+
+  },
+
 }
